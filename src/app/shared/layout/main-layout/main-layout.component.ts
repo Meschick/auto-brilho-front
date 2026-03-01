62aedfd1dc4574168c7bbd78b92e9fe1144540a8
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2026-03-01 15:35:34 -0300

    feat: Nova estrutura

--- src/app/shared/layout/main-layout/main-layout.component.ts
+++ src/app/shared/layout/main-layout/main-layout.component.ts
@@ -3,12 +3,14 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [
     SidebarComponent,
+    HeaderComponent,
     CommonModule,
     RouterOutlet
   ],
