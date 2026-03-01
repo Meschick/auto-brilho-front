64931a71f05a2ee2958aeb25ebd4d9d3f94a42e3
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2026-03-01 15:36:06 -0300

    feat: Criar arquivo de componentes globais

---
+++ src/app/core/angular-material/angular-material.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export const ANGULAR_MATERIAL = [
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  RouterModule,
+  MatButtonModule,
+  MatIconModule
+]
