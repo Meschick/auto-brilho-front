a6323d116d5effc1de779fdfe287f2d4682d82ef
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2026-03-01 15:33:54 -0300

    feat: Criando componente de header

---
+++ src/app/shared/layout/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ANGULAR_MATERIAL } from '../../../../core/angular-material/angular-material';
+
+@Component({
+  selector: 'app-header',
+  imports: [ANGULAR_MATERIAL],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
